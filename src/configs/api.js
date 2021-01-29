4193d322887290b195c6c9513fc0f1c51ac83738
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-01-28 21:15:04 -0400

    changed URL api to ip forein agora simmmmmm

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,4 +1,4 @@
 /*Production*/
-export const URI_BASE_API = 'http://187.6.254.170:8181//teleman/api'
+export const URI_BASE_API = 'http://187.6.254.170:8181/teleman/api'
 /*Dev*/
 //  export const URI_BASE_API = 'http://localhost:8080/api'

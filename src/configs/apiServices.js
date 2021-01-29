eea92ac0fd369a98895318ae4161ba5baf12f49b
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-01-29 09:48:53 -0400

    implemented gabis to harddddd

--- src/configs/apiServices.js
+++ src/configs/apiServices.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 async function apiPublic() {
     const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
     const splited = response.data.ip.split('.')
-    const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'http://187.6.254.170:8181/teleman/api';
+    const url = splited[0] == '172' ? 'https://cors-anywhere.herokuapp.com/http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
+    console.log('setUrl: ', url);
     return axios.create({
         baseURL: url,
         timeout: 50000

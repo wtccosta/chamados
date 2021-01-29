445313f15e85eba0f8d2ae3ea61a4f0cc6f63bb4
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-01-28 22:47:26 -0400

    fixed pmc name on footer

--- src/components/ChamadosFooter.vue
+++ src/components/ChamadosFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer d-flex-column pt-2">
         <div class="upper d-flex justify-content-center">Superintendência de Tecnologia da Informação e Comunicação</div>
-        <div class="down d-flex justify-content-center">Prefeitura Municiapal de Corumbá</div>
+        <div class="down d-flex justify-content-center">Prefeitura Municipal de Corumbá</div>
 
     </div>
 </template>

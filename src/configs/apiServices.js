c5d6d420e9a4adb78f37bdc511e332c250a3c2fb
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-01-29 09:19:45 -0400

    implemented gabis to select url of api hard mode kkk

--- src/configs/apiServices.js
+++ src/configs/apiServices.js
@@ -14,7 +14,7 @@ async function apiPublic() {
 async function apiProtected() {
     const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
     const splited = response.data.ip.split('.')
-    const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'http://187.6.254.170:8181/teleman/api';
+    const url = splited[0] == '172' ? 'https://cors-anywhere.herokuapp.com/http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
     return axios.create({
         baseURL: url,
         timeout: 50000,

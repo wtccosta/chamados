7b41eae5cba84f60a9c5832cf7be6e72a17757c1
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-02-08 09:57:57 -0400

    mudança endereco

--- src/configs/apiServices.js
+++ src/configs/apiServices.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 async function apiPublic() {
-    //const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
-    //const splited = response.data.ip.split('.')
-    //const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
-    const url = 'http://187.6.254.170:8181/teleman/api';
+    const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
+    const splited = response.data.ip.split('.')
+    const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
+    // const url = 'http://187.6.254.170:8181/teleman/api';
     //const url = 'http://localhost:8080/api'
      return axios.create({
         baseURL: url,
@@ -14,10 +14,10 @@ async function apiPublic() {
 
 
 async function apiProtected() {
-    //const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
-    //const splited = response.data.ip.split('.')
-    //const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
-    const url = 'http://187.6.254.170:8181/teleman/api';
+    const response = await axios.get('https://cors-anywhere.herokuapp.com/https://api64.ipify.org?format=json');
+    const splited = response.data.ip.split('.')
+    const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'https://cors-anywhere.herokuapp.com/http://187.6.254.170:8181/teleman/api';
+    // const url = 'http://187.6.254.170:8181/teleman/api';
    //const url = 'http://localhost:8080/api'
     return axios.create({
         baseURL: url,

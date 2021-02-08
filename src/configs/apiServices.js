8aed173549724778af0c9c671376a33d28733c10
Wardes Thiago Costa <wardes@pop-os.localdomain>
2021-02-08 10:19:50 -0400

    mudança endereco novamente, sem proxy reverso 3

--- src/configs/apiServices.js
+++ src/configs/apiServices.js
@@ -4,7 +4,7 @@ async function apiPublic() {
     const response = await axios.get('https://api64.ipify.org?format=json');
     const splited = response.data.ip.split('.')
     const url = splited[0] == '172' ? 'http://172.16.0.35:8181/teleman/api' : 'http://187.6.254.170:8181/teleman/api';
-    console.log(url);
+    console.log(splited);
     // const url = 'http://187.6.254.170:8181/teleman/api';
     //const url = 'http://localhost:8080/api'
      return axios.create({
